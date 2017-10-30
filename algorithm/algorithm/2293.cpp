42d45072e0612d205235e40e8b545b851def0ded
SJW <SJW@DESKTOP-125KIL2>
2017-10-31 01:03:02 +0900

    z

--- algorithm/algorithm/2293.cpp
+++ algorithm/algorithm/2293.cpp
@@ -1,12 +1,25 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-    int n, k;
-    int dp[100];
-    cin >> n >> k;
-
-
-
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <stdio.h>
+//using namespace std;
+//
+//int main(void) {
+//    int n, k;
+//    char *body = "name=joowookzzang&time=1000.200012&value=11.111100";
+//    char name[30];
+//    double time=0;
+//    double value=0;
+//    strtok(body, "=");
+//    name = strtok(NULL, "");
+//
+//    sscanf(body, "name=%s", name);
+//    sscanf(name, "&time=%lf", time);
+//    printf("%s\n", name);
+//    printf("%s\n", time);
+//    printf("%s\n", value);
+//
+//    getchar();
+//    getchar();
+//
+//    return 0;
+//}
---
+++ algorithm/algorithm/2965.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//    int A, B, C;
+//    scanf("%d %d %d", &A, &B, &C);
+//
+//    printf("%d", B - A > C - B ? B - A -1: C - B -1);
+//
+//    return 0;
+//}
